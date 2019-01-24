3eb505499df5dcb85bb84ba0a1a1cbac1aec76b3
Praditya Rendi 28 <praditya.rendi88@gmail.com>
2019-01-24 19:36:26 +0700

    App Bar with a primary

--- src/Counter/Home.js
+++ src/Counter/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Appbar1 from '../Components/Appbar/App bar with button/Appbar1';
 import Appbar2 from '../Components/Appbar/Simple App Bar/Appbar2';
+import Appbar3 from '../Components/Appbar/App bar with Primary/Appbar3';
 class Home extends Component {
   render() {
     return (
@@ -8,6 +9,8 @@ class Home extends Component {
         <Appbar1 />
         <br />
         <Appbar2 />
+        <br />
+        <Appbar3 />
       </div>
     );
   }
