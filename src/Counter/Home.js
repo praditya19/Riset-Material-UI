3fe8ebbf84105920cf1bf1ed119e9e3fad492d8b
Praditya Rendi 28 <praditya.rendi88@gmail.com>
2019-01-24 17:56:36 +0700

    App bar with button

---
+++ src/Counter/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Appbar1 from '../Components/Appbar/App bar with button/Appbar1';
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Appbar1 />
+      </div>
+    );
+  }
+}
+export default Home;
