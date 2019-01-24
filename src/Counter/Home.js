77053890420d1efa632c62f39bdeed96090a6ccb
Praditya Rendi 28 <praditya.rendi88@gmail.com>
2019-01-24 20:46:01 +0700

    Image Avatars

--- src/Counter/Home.js
+++ src/Counter/Home.js
@@ -5,12 +5,13 @@ import React, { Component } from 'react';
 //import Appbar4 from '../Components/Appbar/App Bar with menu/Appbar4';
 //import Appbar5 from '../Components/Appbar/App Bar with search/Appbar5';
 //import Appbar6 from '../Components/Appbar/desktop only/Appbar6';
-import Appbar7 from '../Components/Appbar/Bottom App Bar/Appbar7';
+//import Appbar7 from '../Components/Appbar/Bottom App Bar/Appbar7';
+import Av1 from '../Components/Avatar/Image avatars/Av1';
 class Home extends Component {
   render() {
     return (
       <div>
-        <Appbar7 />
+        <Av1 />
       </div>
     );
   }
