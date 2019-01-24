2887e1ed7554e9cef9d0c68f3e7ea8b3dad360b8
Praditya Rendi 28 <praditya.rendi88@gmail.com>
2019-01-24 20:53:17 +0700

    Letter avatars

--- src/Counter/Home.js
+++ src/Counter/Home.js
@@ -6,12 +6,14 @@ import React, { Component } from 'react';
 //import Appbar5 from '../Components/Appbar/App Bar with search/Appbar5';
 //import Appbar6 from '../Components/Appbar/desktop only/Appbar6';
 //import Appbar7 from '../Components/Appbar/Bottom App Bar/Appbar7';
-import Av1 from '../Components/Avatar/Image avatars/Av1';
+//import Av1 from '../Components/Avatar/Image avatars/Av1';
+import Av2 from '../Components/Avatar/Letter avatars/Av2';
+import Av3 from '../Components/Avatar/Icon avatars/Av3';
 class Home extends Component {
   render() {
     return (
       <div>
-        <Av1 />
+        <Av3 />
       </div>
     );
   }
