00aa7b1efa1c0ef2081e86624a875ed026113545
Praditya Rendi 28 <praditya.rendi88@gmail.com>
2019-01-24 18:15:16 +0700

    Simple App Bar

--- src/Counter/Home.js
+++ src/Counter/Home.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import Appbar1 from '../Components/Appbar/App bar with button/Appbar1';
+import Appbar2 from '../Components/Appbar/Simple App Bar/Appbar2';
 class Home extends Component {
   render() {
     return (
       <div>
         <Appbar1 />
+        <br />
+        <Appbar2 />
       </div>
     );
   }
