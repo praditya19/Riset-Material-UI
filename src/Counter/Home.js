aa76d3ca7693fdf1ee84c1e74929ee14fc3f2547
Praditya Rendi 28 <praditya.rendi88@gmail.com>
2019-01-24 19:50:50 +0700

    App Bar with search

--- src/Counter/Home.js
+++ src/Counter/Home.js
@@ -3,6 +3,8 @@ import Appbar1 from '../Components/Appbar/App bar with button/Appbar1';
 import Appbar2 from '../Components/Appbar/Simple App Bar/Appbar2';
 import Appbar3 from '../Components/Appbar/App bar with Primary/Appbar3';
 import Appbar4 from '../Components/Appbar/App Bar with menu/Appbar4';
+import Appbar5 from '../Components/Appbar/App Bar with search/Appbar5';
+import Appbar6 from '../Components/Appbar/desktop only/Appbar6';
 class Home extends Component {
   render() {
     return (
@@ -14,6 +16,10 @@ class Home extends Component {
         <Appbar3 />
         <br />
         <Appbar4 />
+        <br />
+        <Appbar5 />
+        <br />
+        <Appbar6 />
       </div>
     );
   }
